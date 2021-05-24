918f207721d0b54cb628d4ab960fe91f5a9e138a
maggie <maggie.tamanini@gmail.com>
2021-05-24 14:03:23 -0400

    Start of code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace Lab_Room_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("Welcome to your personal Room Design Calculator");
+            Console.WriteLine("");
+            Console.WriteLine("Please enter imperial units in feet.");
+            Console.WriteLine("");
+
+
+            Console.Write("Enter Length:");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter Width:");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area: " + width * length );
+
+            double halfPer = width + length;
+            double perimeter = halfPer *2;
+
+            Console.WriteLine("Perimeter: " + perimeter);
+
+
+              
+
+           
+
+
         }
     }
 }
