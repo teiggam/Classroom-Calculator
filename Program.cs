d61fb12a47721ec8f4fb521bba154a33119980b8
maggie <maggie.tamanini@gmail.com>
2021-05-24 15:14:43 -0400

    Extended Exercises Finalized

--- Program.cs
+++ Program.cs
@@ -10,29 +10,43 @@ namespace Lab_Room_Calculator
 
             Console.WriteLine("Welcome to your personal Room Design Calculator");
             Console.WriteLine("");
-            Console.WriteLine("Please enter imperial units in feet.");
-            Console.WriteLine("");
+            Console.WriteLine("Please enter measurement in feet.");
+
+            string again;
+            string stop;
+            do
+            {
 
 
-            Console.Write("Enter Length:");
-            double length = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                Console.Write("Enter Length: ");
+                double length = double.Parse(Console.ReadLine());
+                Console.Write("Enter Width: ");
+                double width = double.Parse(Console.ReadLine());
+                Console.Write("Enter Height: ");
+                double height = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
 
-            Console.Write("Enter Width:");
-            double width = double.Parse(Console.ReadLine());
+                double area = width * length;
+                double perimeter = width + length * 2;
+                double volume = width * length * height;
 
-            Console.WriteLine("Area: " + width * length );
+                Console.WriteLine("The area is " + area + " square feet.");
+                Console.WriteLine("The perimeter is " + perimeter + " feet.");
+                Console.WriteLine("The volume of the room is: " + volume + " cubic feet.");
+                Console.WriteLine("Carpet Tiles Needed: " + area / 5);
+                Console.WriteLine("Cans of Paint Needed: " + perimeter / 5);
+                Console.WriteLine("");
 
-            double halfPer = width + length;
-            double perimeter = halfPer *2;
+                Console.WriteLine("Would you like to calculate another room? (Y/N): ");
 
-            Console.WriteLine("Perimeter: " + perimeter);
+                again = Console.ReadLine();
 
+            }
 
-              
-
-           
+            while (string.Equals(again, "Y", StringComparison.OrdinalIgnoreCase));
 
 
-        }
+         }
     }
 }
